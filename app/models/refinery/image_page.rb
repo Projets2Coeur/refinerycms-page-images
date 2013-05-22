b610034ce3f4edeb8197ac5a4aab8dc37b1c61b2
Jérémy FRERE <frere.jeremy@gmail.com>
2013-05-22 20:16:47 +0200

    Adds caption back to attr_accessible and adds photoby/yellowkorner_url translation

--- app/models/refinery/image_page.rb
+++ app/models/refinery/image_page.rb
@@ -4,9 +4,9 @@ module Refinery
     belongs_to :image
     belongs_to :page, :polymorphic => true
 
-    translates :caption, :title if self.respond_to?(:translates)
+    translates :caption, :title, :photoby_url, :yellowkorner_url if self.respond_to?(:translates)
 
-    attr_accessible :image_id, :position, :locale, :title, :photoby_url, :yellowkorner_url
+    attr_accessible :image_id, :position, :locale, :caption, :title, :photoby_url, :yellowkorner_url
     self.translation_class.send :attr_accessible, :locale
 
   end
